22c3a4c82dde260c02857033580c6e127a8b2662
Nick Maclean <nicolasimaclean@gmail.com>
2022-05-17 16:33:26 -0500

    Add: Generic Extension Method
    IsNull returns true if value is null.

---
+++ Runtime/Core/Extensions/GenericExtensions.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Gummi
+{
+    public static class GenericExtensions
+    {
+        public static bool IsNull<T>(this T val)
+        {
+            var type = typeof(T);
+
+            bool isNullable = type.IsClass || Nullable.GetUnderlyingType(type) != null; 
+            return isNullable && EqualityComparer<T>.Default.Equals(val, default(T));
+        }
+    }
+}
