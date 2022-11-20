542f7257e9e9a6946dfb5ceec8781a8ce7739a57
Nick Maclean <nicolasimaclean@gmail.com>
2022-11-19 19:17:34 -0600

    FloatSquared
    FloatSquared computes value squared in editor (instead of runtime) while providing the interface (in editor) as a float

---
+++ Editor/Drawers/FloatSquaredDrawer.cs
@@ -0,0 +1,34 @@
+﻿using UnityEditor;
+using UnityEngine;
+using Gummi.Utility;
+
+namespace GummiEditor.Drawers
+{
+    [CustomPropertyDrawer(typeof(FloatSquared))]
+    public class FloatSquaredDrawer : PropertyDrawer
+    {
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            var valueProperty = property.FindPropertyRelative(nameof(FloatSquared._value));
+            return EditorGUI.GetPropertyHeight(valueProperty);
+        }
+
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            var valueProperty = property.FindPropertyRelative(nameof(FloatSquared._value));
+
+            EditorGUI.BeginChangeCheck();
+            
+            EditorGUI.BeginProperty(position, label, property);
+            EditorGUI.PropertyField(position, valueProperty, label, true);
+            EditorGUI.EndProperty();
+
+            if (EditorGUI.EndChangeCheck())
+            {
+                float value = valueProperty.floatValue;
+                var valueSQProperty = property.FindPropertyRelative(nameof(FloatSquared._valueSQ));
+                valueSQProperty.floatValue = value * value;
+            }
+        }
+    }
+}
---
+++ Runtime/Utility/FloatSquared.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Gummi.Utility
+{
+    [System.Serializable]
+    public struct FloatSquared
+    {
+        public float Value => _value;
+        public float ValueSQ => _valueSQ;
+        
+        [SerializeField, Min(0)] internal float _value;
+        [SerializeField] internal float _valueSQ;
+
+        public FloatSquared(float value)
+        {
+            _value = value;
+            _valueSQ = value * value;
+        }
+    }
+}
